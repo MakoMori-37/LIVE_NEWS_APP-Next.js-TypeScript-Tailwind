24fb2f1ef16517ae992afd44b961b708aa91aefa
MakoMori-37 <oun_kup@hotmail.com>
2023-01-10 17:10:02 +0700

    create mock data

--- libs/fetchNewsByCategory.ts
+++ libs/fetchNewsByCategory.ts
@@ -1,13 +1,38 @@
-import axios from "axios"
-
-import sortNewsByImage from "./sortNewsByImage"
+// import axios from "axios"
+import { businessData, entertainmentData, generalData, healthData, scienceData, sportsData, technologyData } from '../mockData'
+// import sortNewsByImage from "./sortNewsByImage"
 
 const fetchNewsByCategory = async (category: string) => {
-    console.log({ category })
     try {
-        const { data } = await axios.get(`${process.env.NEXT_PUBLIC_MEDIASTACK_URL}news?access_key=${process.env.NEXT_PUBLIC_MEDIASTACK_API_KEY}&categories=${category}`)
-        const news = sortNewsByImage(data)
-        return news
+        // const { data } = await axios.get(`${process.env.NEXT_PUBLIC_MEDIASTACK_URL}news?access_key=${process.env.NEXT_PUBLIC_MEDIASTACK_API_KEY}&categories=${category}`)
+        // const news = sortNewsByImage(data)
+        // return news
+        switch (category) {
+            case 'business':
+                return businessData
+                break;
+            case 'entertainment':
+                return entertainmentData
+                break;
+            case 'general':
+                return generalData
+                break;
+            case 'health':
+                return healthData
+                break;
+            case 'science':
+                return scienceData
+                break;
+            case 'sports':
+                return sportsData
+                break;
+            case 'technology':
+                return technologyData
+                break;
+            default:
+                return generalData
+                break;
+        }
     } catch (error) {
         console.log(error)
     }
